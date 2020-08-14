d5dfd61cdf7dd83f379f95be4a358bb38c0a18c1
Jaffar Abbas <gamaportal8@gmail.com>
2020-08-14 16:21:54 +0500

    14-8-2020  main js file added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// $(document).ready(function() {
+//     $('#product').change(function() {
+//         var course_id = $(this).val();
+//         $.ajax({
+//             url: "index.php",
+//             method: "POST",
+//             data: { courseID: course_id },
+//             success: function(data) {
+//                 $("#product_Type").html(data);
+//             }
+//         });
+//     });
+// });
+
+
+$(document).ready(function() {
+    alert("sdfsdf");
+});
